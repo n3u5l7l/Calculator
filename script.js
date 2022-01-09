a5ac7d68302c784636756951deb015a317d2a252
MingMing <greeteaz567@gmail.com>
2022-01-09 00:58:15 -0600

    Added operate() function

--- script.js
+++ script.js
@@ -14,3 +14,23 @@ function divide(num1,num2)
 {
   return (num1/num2).toFixed(2);
 }
+
+function operate(num1, operation, num2)
+{
+  if(operation==="divide")
+  {
+    return divide(num1,num2);
+  }
+  else if(operation==="mulitply")
+  {
+    return mulitply(num1,num2);
+  }
+  else if(operation==="subtract")
+  {
+    return subtract(num1,num2);
+  }
+  else if(operation==="add")
+  {
+    return add(num1,num2);
+  }
+}
