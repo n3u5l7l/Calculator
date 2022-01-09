9d30fb9f5667dc4b8f8ab79f00221f19e76602a2
MingMing <greeteaz567@gmail.com>
2022-01-09 00:57:10 -0600

    Added operation functions

---
+++ script.js
@@ -0,0 +1,16 @@
+function add(num1,num2)
+{
+  return (num1+num2).toFixed(2);
+}
+function subtract(num1, num2)
+{
+  return (num1-num2).toFixed(2);
+}
+function mulitply(num1,num2)
+{
+  return (num1*num2).toFixed(2);
+}
+function divide(num1,num2)
+{
+  return (num1/num2).toFixed(2);
+}
